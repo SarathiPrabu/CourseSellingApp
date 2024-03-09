a5331ef6d67987db51c1e7af7230f786f2e1e642
Sarathi Prabu <sarathiprabum@gmail.com>
2024-03-09 01:37:21 -0500

    Add database connection and remove unnecessary code

--- server/index.js
+++ server/index.js
@@ -1,23 +1,18 @@
 const express = require('express');
-// const mongoose = require('mongoose');
 const config = require('./config/config');
+const connectDB = require('./models/database');
 const adminRoutes = require('./routes/adminRoutes');
 const userRoutes = require('./routes/userRoutes');
+
+// Connect to the database
+connectDB();
 
 const app = express();
 app.use(express.json());
 
-// // Mongoose connection
-// const dbURI = config.dbURI;
-// mongoose.connect(dbURI)
-//     .then(() => console.log('MongoDB connected...'))
-//     .catch(err => console.error('MongoDB connection error:', err));
-
 // Routes
 app.use('/admin', adminRoutes);
-
 // app.use('/user', userRoutes);
-
 
 // Port listener for the server to run on defined port
 app.listen(config.port, () => {
